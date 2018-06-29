9aed5d06d3f715573f40138bfbbaba8ccec9d337
Idris Olayinka <atinukeolayinkaid@gmail.com>
2018-06-30 00:25:53 +0100

    Initial commit

---
+++ design.js
@@ -0,0 +1,29 @@
+
+
+let height, width;
+
+
+$('#sizePicker').submit(function(event) {
+	event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWidth').val();
+	makeGrid(height,width);
+});
+
+function makeGrid(x, y) {
+	$('tr').remove();
+	for (let r = 1; r <= x; r++) {
+		$('#pixelCanvas').append('<tr id=table' + r + '></tr>');
+		for (let c = 1; c <= y; c++) {
+			$('#table' + r).append('<td></td>');
+		}
+	}
+$('#pixelCanvas').click(function(event) {
+	let color = $('#colorPicker').val();
+	$(event.target).css('background-color',color);
+	$('td').mouseup(function(){
+    alert('Feel free to color the canvas!');
+    }); 
+});
+ 
+}
